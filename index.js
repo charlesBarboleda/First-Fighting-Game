a650918e28cae19c1589ab65d8c058d4879d5c2b
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2024-04-23 07:26:34 -0700

    Init sprites
    init sprites

--- index.js
+++ index.js
@@ -4,5 +4,43 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillRect(0, 0, canvas.width, canvas.height);
-c.fillStyle = 'black';
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position, velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.fillStyle = 'blue';
+        c.fillRect(this.position.x, this.position.y, 50, 150);
+    }
+}
+
+const player = new Sprite({ 
+    x: 0, 
+    y: 0 
+});
+
+const enemy = new Sprite({ 
+    x: 900, 
+    y: 400 
+});
+
+enemy.draw();
+
+player.draw();
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate);
+
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    enemy.draw();
+
+}
+
+animate();

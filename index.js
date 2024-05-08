c85ae890f47eb0d64be19591bdb668892e47e8c4
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2024-05-08 11:03:18 -0700

    Added background sprite

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-const gravity = 0.4
+const gravity = 0.75
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/background.png',
+})
 
 const player = new Fighter({
     position:{ 
@@ -99,6 +107,8 @@ function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = "black";
     c.fillRect(0, 0, canvas.width, canvas.height);
+    
+    background.update();
     player.update();
     enemy.update();
 
--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,14 @@
 class Sprite {
-    constructor({ position }) {
+    constructor({ position, imageSrc }) {
         this.position = position
         this.width = 1024
         this.height = 576
+        this.image = new Image()
+        this.image.src = imageSrc
     }
 
     draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     }
 
     update() {
